d2e06f2aee27cc161578d1548af58e1dc7387e53
budi suryadi <bud1.5ury4d1@gmail.com>
2026-06-11 21:54:58 +0700

    eat: add NASA API configuration

--- src/main/java/com/budi/asteroid/tracker/AsteroidTrackerApplication.java
+++ src/main/java/com/budi/asteroid/tracker/AsteroidTrackerApplication.java
@@ -1,9 +1,12 @@
 package com.budi.asteroid.tracker;
 
+import com.budi.asteroid.tracker.config.NasaApiProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({NasaApiProperties.class})
 public class AsteroidTrackerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/budi/asteroid/tracker/config/NasaApiProperties.java
+++ src/main/java/com/budi/asteroid/tracker/config/NasaApiProperties.java
@@ -1,4 +1,13 @@
 package com.budi.asteroid.tracker.config;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "nasa.api")
 public class NasaApiProperties {
+    private String baseUrl;
+    private String key;
 }
