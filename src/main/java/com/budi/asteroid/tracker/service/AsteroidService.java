04d88e7670d4ed78dfb99c8b1eb81de03554d6e2
budi suryadi <bud1.5ury4d1@gmail.com>
2026-06-12 15:33:52 +0700

    feat: implement asteroid aggregation logic

--- src/main/java/com/budi/asteroid/tracker/service/AsteroidService.java
+++ src/main/java/com/budi/asteroid/tracker/service/AsteroidService.java
@@ -1,6 +1,10 @@
 package com.budi.asteroid.tracker.service;
 
+import com.budi.asteroid.tracker.client.NasaApiClient;
+import com.budi.asteroid.tracker.client.dto.NasaNeoFeedResponse;
 import com.budi.asteroid.tracker.dto.AsteroidResponse;
+import com.budi.asteroid.tracker.validator.DateRangeValidator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -9,9 +13,42 @@ import java.util.List;
 @Service
 public class AsteroidService {
 
-    public List<AsteroidResponse> findClosestAsteroids(LocalDate startDate,
-                                                       LocalDate endDate) {
-        return List.of();
+    private final NasaApiClient nasaApiClient;
+    private final DateRangeValidator dateRangeValidator;
+
+    @Autowired
+    public AsteroidService(NasaApiClient nasaApiClient, DateRangeValidator dateRangeValidator) {
+        this.nasaApiClient = nasaApiClient;
+        this.dateRangeValidator = dateRangeValidator;
+    }
+
+    public List<AsteroidResponse> findClosestAsteroids(LocalDate startDate,LocalDate endDate) {
+        dateRangeValidator.validate(startDate, endDate);
+
+        NasaNeoFeedResponse response = nasaApiClient.getNeoFeed(startDate,endDate);
+        if (response.getNearEarthObjects() == null) {
+            return List.of();
+        }
+
+        return response.getNearEarthObjects()
+                .values()
+                .stream()
+                .flatMap(List::stream)
+                .map(this::toResponse)
+                .toList();
+    }
+
+    private AsteroidResponse toResponse(NasaNeoFeedResponse.NasaAsteroid asteroid) {
+        NasaNeoFeedResponse.CloseApproachData closeApproachData = asteroid.getCloseApproachData().get(0);
+
+        return AsteroidResponse.builder()
+                .id(asteroid.getId())
+                .name(asteroid.getName())
+                .missDistanceKm(closeApproachData.getMissDistance().getKilometers())
+                .estimatedDiameterMinMeters(asteroid.getEstimatedDiameter().getMeters().getMin())
+                .estimatedDiameterMaxMeters(asteroid.getEstimatedDiameter().getMeters().getMax())
+                .potentiallyHazardous(asteroid.getPotentiallyHazardous())
+                .build();
     }
 
 }
